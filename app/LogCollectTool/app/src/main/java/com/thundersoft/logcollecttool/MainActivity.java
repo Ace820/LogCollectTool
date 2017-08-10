7b130af8b27a7a2791fda99446310569bb1261c1
Ace <142287@gmail.com>
2017-08-11 00:26:21 +0800

    Fix can not open application
    [Author] Ace
    [Note] 1.set checkbox[5] ,but actually lower than 5.caused by not remove
    magic number when add new logic.
    2.add save to sharedpreference file logic.

--- app/LogCollectTool/app/src/main/java/com/thundersoft/logcollecttool/MainActivity.java
+++ app/LogCollectTool/app/src/main/java/com/thundersoft/logcollecttool/MainActivity.java
@@ -2,6 +2,7 @@ package com.thundersoft.logcollecttool;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -13,6 +14,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,25 +34,35 @@ public class MainActivity extends AppCompatActivity {
 
     protected void createView() {
         RelativeLayout frameLayout = new RelativeLayout(this);
-        LogType[] checkBox =new LogType[5];
         Button getProfile = new Button(this);
         Button uploadNow = new Button(this);
         SharedPreferences sP = getSharedPreferences("logType",Activity.MODE_PRIVATE);
         String names = sP.getString("name","");
         String properties = sP.getString("property","");
-
-        String[] name = names.split(" ");
-        String[] property = properties.split(" ");
-        if (name.length >0) {
+        String[] name,property;
+        if (names.equals("")){
+            //TODO: may need a dialog to ask if need to import a profile
+            name = new String[]{"kernel log","logcat"};
+            property = new String[]{"persist.sys.cKLog","persist.sys.cLogcat"};
+        } else {
+            name = names.split(":");
+            property = properties.split(":");
+        }
+        LogType[] checkBox =new LogType[name.length];
+        Log.d(TAG,"name length="+name.length);
+        if (name.length >1) {
             for (int i = 0; i < name.length; i++) {
                 checkBox[i] = new LogType(name[i], property[i], i, this);
             }
-            //TODO adjust if the input object number is 0 or 1
             if (checkBox.length != 1) {
-                for (int i = (checkBox.length / 2 - 1); i >= 0; i--)
+                for (int i = (checkBox.length / 2 - 1); i >= 0; i--) {
                     checkBox[i].parms.addRule(RelativeLayout.ABOVE, checkBox[i + 1].id);
-                for (int i = (checkBox.length / 2 + 1); i < checkBox.length; i++)
+                    checkBox[i].parms.addRule(RelativeLayout.ALIGN_LEFT, checkBox[i + 1].id);
+                }
+                for (int i = (checkBox.length / 2 + 1); i < checkBox.length; i++) {
                     checkBox[i].parms.addRule(RelativeLayout.BELOW, checkBox[i - 1].id);
+                    checkBox[i].parms.addRule(RelativeLayout.ALIGN_LEFT, checkBox[i - 1].id);
+                }
                 for (int i = 0; i < name.length; i++) {
                     frameLayout.addView(checkBox[i].checkBox);
                 }
@@ -102,15 +118,42 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode,int resultCode,Intent data) {
         Uri uri;
+        String tempStr;
+        String name = "";
+        String property = "";
+        SharedPreferences preferences = getSharedPreferences("logType", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
 
         super.onActivityResult(requestCode,resultCode,data);
         if (resultCode != Activity.RESULT_OK) {
             Log.e(TAG,"Choose file error,error code is "+resultCode);
             return;
         }
-        if (requestCode == 0) {
-            uri = data.getData();
-            Log.d(TAG,"get file path:"+uri.getPath());
+        uri = data.getData();
+        Log.d(TAG,"get file path:"+uri.getPath());
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(uri.getPath())));
+            if (!reader.readLine().equals("LogCollectTool")){
+                //TODO start a Dialog to warn not correct file
+                return;
+            }
+            while ((tempStr = reader.readLine()) != null ) {
+                String[] value = tempStr.split("|");
+                if (value.length !=2 ){
+                    //TODO start a Dialog to warn line error
+                } else {
+                    name += value[0];
+                    property += value[1];
+                }
+            }
+            if (!name.equals("")) {
+                editor.putString("name", name);
+                editor.putString("property", property);
+            }
+            editor.commit();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
