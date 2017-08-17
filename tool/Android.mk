43489d7aa0ec47e135aac64509813ff925b0a6db
zhangche0715 <zhangche0715@thundersoft.com>
2017-08-17 09:51:10 +0800

    Add Android.mk to compile APP in android source code
    [Author] Ace
    [Note] Copy from net,but occur some errors:can not find Mainactivity.
    Add LOCAL_MODULE_TAGS := optional to solve it

---
+++ tool/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := LogCollectTool
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-gridlayout
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
+
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/appcompat/res
+LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/gridlayout/res
+
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat:android.support.v7.gridlayout
+
+include $(BUILD_PACKAGE)
